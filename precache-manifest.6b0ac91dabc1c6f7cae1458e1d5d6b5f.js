eddf6a05e1d83d24234b0fc04d3da999504d7a41
LossonLuckett <lossonluckett1@gmail.com>
2020-07-23 22:09:54 -0400

    Updates

---
+++ precache-manifest.6b0ac91dabc1c6f7cae1458e1d5d6b5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64167eb4d31516a77097dd7ffde7268",
+    "url": "/Portfolio_Site/index.html"
+  },
+  {
+    "revision": "d0d6750aa6eeec4097d8",
+    "url": "/Portfolio_Site/static/css/main.9a321771.chunk.css"
+  },
+  {
+    "revision": "7b3d25203bea471dcd78",
+    "url": "/Portfolio_Site/static/js/2.e2331c1b.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/Portfolio_Site/static/js/2.e2331c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d6750aa6eeec4097d8",
+    "url": "/Portfolio_Site/static/js/main.ca2bcff4.chunk.js"
+  },
+  {
+    "revision": "9f458a96f6637d88f1a6",
+    "url": "/Portfolio_Site/static/js/runtime-main.d37f5ed7.js"
+  },
+  {
+    "revision": "6aa3363a774adfefee731d353efd24bc",
+    "url": "/Portfolio_Site/static/media/Avatar.6aa3363a.png"
+  },
+  {
+    "revision": "a72264f4eeb1b74f17c77a18b73f0c97",
+    "url": "/Portfolio_Site/static/media/back.a72264f4.jpg"
+  },
+  {
+    "revision": "7b1402d0dd9cd6b5ac6feb41d4cf01ec",
+    "url": "/Portfolio_Site/static/media/cs_logo.7b1402d0.png"
+  },
+  {
+    "revision": "b18fd8d57290e40a045f114f231c7fcc",
+    "url": "/Portfolio_Site/static/media/pythonlogomaster.b18fd8d5.png"
+  },
+  {
+    "revision": "6c234956df34c7efeb1617e1c7e1eff7",
+    "url": "/Portfolio_Site/static/media/react-material-ui-main.6c234956.jpg"
+  }
+]);
